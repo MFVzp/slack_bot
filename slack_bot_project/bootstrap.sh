345c50ac1c64e0e6baa092db2524aae631830931
MFVzp <tauruszpua@yahoo.com>
2017-12-04 03:13:56 +0200

    move deploy files

--- slack_bot_project/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install python3-pip -y
-cd /vagrant/
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
