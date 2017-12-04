c86301d4ed0db3862b51e787d78e9c4d9408d810
MFVzp <tauruszpua@yahoo.com>
2017-12-04 04:45:32 +0200

    change settings

--- slack_bot_project/settings.py
+++ slack_bot_project/settings.py
@@ -135,9 +135,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
