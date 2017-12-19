f5b779aa870a7c5d38faa54ec214fd02cf5c3987
MFVzp <tauruszpua@yahoo.com>
2017-12-19 13:02:06 +0200

    add team_app_api with REST

--- slack_bot_project/settings.py
+++ slack_bot_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'debug_toolbar',
 
     'ask_leave_bot',
---
+++ team_app_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TeamAppApiConfig(AppConfig):
+    name = 'team_app_api'
